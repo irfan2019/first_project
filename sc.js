6e8cdfa114ea614dfe36a79546610462110221ea
irfan2019 <sam20will@gmail.com>
2022-06-06 18:32:11 +0530

    successfully added

---
+++ sc.js
@@ -0,0 +1,21 @@
+var section=document.querySelectorAll('.section');
+          var sections={};
+          section.forEach(function(e)
+                          {
+          console.log(e.id,e.offsetTop)
+              sections[e.id]=e.offsetTop
+          }
+        )
+          window.onscroll=function()
+          {
+              var scrollpos=document.documentElement.scrollTop || document.body.scrollTop;
+              console.log(scrollpos);
+              for(var x in sections)
+                  {
+                      if(sections[x]<=scrollpos)
+                          {
+                              document.querySelector('active').setAttribute('class','');
+                              document.querySelector('a[href*='+x+']').setAttribute('class','active')
+                          }
+                  }
+          }
